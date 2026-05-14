fe9f65da08113895053080ac919451c27d46a38a
Nxstairs <n.shota1624@gmail.com>
2026-05-14 15:42:12 +0900

    update page

--- js/main.js
+++ js/main.js
@@ -3,6 +3,24 @@ const nav = document.getElementById("nav");
 
 if (menuToggle && nav) {
   menuToggle.addEventListener("click", () => {
-    nav.classList.toggle("active");
+    nav.classList.toggle("open");
   });
 }
+
+const track = document.querySelector(".carypto-track");
+const slides = document.querySelectorAll(".carypto-card");
+
+let index = 0;
+
+setInterval(() => {
+
+  index++;
+
+  if(index >= slides.length){
+    index = 0;
+  }
+
+  track.style.transform =
+    `translateX(-${index * 100}%)`;
+
+}, 3000);
