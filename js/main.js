150587c6b92d218c7de9891ce733a1f1eb185340
Nxstairs <n.shota1624@gmail.com>
2026-05-25 16:46:46 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -7,34 +7,6 @@ if (menuToggle && nav) {
   });
 }
 
-const tracks = document.querySelectorAll(".carypto-track");
-
-tracks.forEach((track) => {
-  const slides = track.querySelectorAll(".carypto-card");
-
-  if (slides.length > 0) {
-    let index = 0;
-
-    setInterval(() => {
-      const slideStyle = getComputedStyle(track);
-      const gap = parseInt(slideStyle.gap) || 0;
-      const slideWidth = slides[0].offsetWidth + gap;
-
-      index++;
-
-      track.style.transition = "transform 0.7s ease";
-      track.style.transform = `translateX(-${index * slideWidth}px)`;
-
-      if (index >= slides.length - 6) {
-        setTimeout(() => {
-          track.style.transition = "none";
-          index = 0;
-          track.style.transform = "translateX(0)";
-        }, 700);
-      }
-    }, 3000);
-  }
-});
 
 const animatedItems = document.querySelectorAll(
   ".slide-up, .slide-left, .slide-right"
